d3ef9ad5e8aa72abb6c82996a289a20f4b64e9a3
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2013-11-04 23:32:41 -0600

    Create workout_jagadeesQeq.m

---
+++ workout_jagadeesQeq.m
@@ -0,0 +1,8 @@
+%WORKOUT_JAGADEESQEQ : workout file of jagadeesQeq
+
+clear all; close all; clc;
+disp('Welcome to jagadeesQeq Demo!')
+a = input('\nPlease enter a, b, c:\n','s');
+coeff = str2num(a);
+x = jagadeesQeq(coeff)
+     
